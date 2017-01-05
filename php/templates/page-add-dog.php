c59338260663ca06ba807b440fecc66aa55a0399
Laura Heino <laura@lauraheino.com>
2017-01-05 10:55:06 +0200

    Testing

--- php/templates/page-add-dog.php
+++ php/templates/page-add-dog.php
@@ -39,7 +39,7 @@ get_header();
            			); ?></p>
            		</div>
            <?php else : ?>
-           	Olet kirjautunut!		
+           	Olet kirjautunut.		
            <?php endif; ?>
 
        </div>
